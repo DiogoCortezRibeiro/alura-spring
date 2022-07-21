c619b63a61f66337e3edd15c8533b6ca43dda3b9
diogo.ribeiro <diogoparceiros@brkambiental.com.br>
2022-07-21 15:02:06 -0300

    Criando get e post da api

--- src/main/java/br/com/alura/linguagens/api/Linguagem.java
+++ src/main/java/br/com/alura/linguagens/api/Linguagem.java
@@ -1,10 +1,20 @@
 package br.com.alura.linguagens.api;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "principaisLinguagens")
 public class Linguagem {
 
+    @Id
+    private String id;
     private String title;
     private String image;
     private int ranking;
+
+    public Linguagem() {
+
+    }
 
     public Linguagem(String title, String image, int ranking) {
         this.title = title;
@@ -22,5 +32,9 @@ public class Linguagem {
 
     public int getRanking() {
         return ranking;
+    }
+
+    public String getId() {
+        return id;
     }
 }
--- src/main/java/br/com/alura/linguagens/api/LinguagemController.java
+++ src/main/java/br/com/alura/linguagens/api/LinguagemController.java
@@ -1,18 +1,26 @@
 package br.com.alura.linguagens.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 public class LinguagemController {
-
-    private List<Linguagem> linguagens;
+    @Autowired
+    private LinguagemRepository linguagemRepository;
 
     @GetMapping(value = "/linguagens")
     public List<Linguagem> obterLinguagens() {
-        return linguagens;
+        return linguagemRepository.findAll();
+    }
+
+    @PostMapping(value = "/linguagens")
+    public Linguagem cadastrarLinguagem(@RequestBody Linguagem linguagem) {
+        return linguagemRepository.save(linguagem);
     }
 
 }
